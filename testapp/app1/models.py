f6fac9139eba33551f4566bcf8ff4deb57c4f125
tarpelite <tarpelite_chan@foxmail.com>
2018-05-07 22:15:11 +0800

    演示markdown

--- testapp/app1/models.py
+++ testapp/app1/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
     name = models.CharField(max_length=40, verbose_name=u'名称')
     parent = models.ForeignKey('self', default=None, blank=True, null=True,
                                verbose_name=u'上级分类', on_delete=models.CASCADE)
-    href = models.CharField(max_length=200, verbose_name=u'链接', blank=True,null=True)
+    href = models.CharField(max_length=200, verbose_name=u'链接', blank=True, null=True)
     rank = models.IntegerField(default=0, verbose_name=u'排序')
     status = models.IntegerField(default=0, choices=STATUS.items(), verbose_name=u'状态')
     create_time = models.DateTimeField(u'创建时间', auto_now_add=True)
@@ -32,6 +32,12 @@ class Category(models.Model):
             return '%s-->%s' % (self.parent, self.name)
         else:
             return '%s' % (self.name)
+
+    def get_parent(self):
+        if self.parent:
+            return True
+        else:
+            return False
 
     __str__ = __unicode__
 
--- testapp/app1/views.py
+++ testapp/app1/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 from django.http import HttpResponse
 import markdown
-from .models import Article
+from .models import Article,Category
 # Create your views here.
 
 
 def base(request):
     return render(request, 'base.html')
 
+def get_catogary():
+    catogary_list = Category.objects.all()
+    bars = []
+    bar_names = []
+    bar_dict = {}
+    for result in catogary_list:
+        if not result.get_parent():
+            bars.append(result)
+            bar_dict[result.name] = []
+
+    for bar in bars:
+        sub_bars = Category.objects.filter(parent=bar)
+        for i in sub_bars:
+            bar_dict[bar.name].append(i.name)
+
 
 def mainview(request, param1):
     article_list = Article.objects.filter(title=param1)
     article = article_list[0]
+    article_path = article.category
     article_body = markdown.markdown(article.content,
                                      extensions=[
                                          'markdown.extensions.extra',
@@ -22,5 +38,6 @@ def mainview(request, param1):
 
     context = {'param1': param1,
                'content': mark_safe(article_body),
+               'path': article_path,
                }
     return render(request, 'mainview.html', context)
