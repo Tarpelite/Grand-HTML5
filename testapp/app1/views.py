d1e110fb6f2884d98b09faccdba1d30b375673ec
tarpelite <tarpelite_chan@foxmail.com>
2018-05-07 23:52:55 +0800

    template changes

--- testapp/app1/views.py
+++ testapp/app1/views.py
@@ -11,19 +11,21 @@ def base(request):
 
 def get_catogary():
     catogary_list = Category.objects.all()
-    bars = []
-    bar_names = []
-    bar_dict = {}
-    for result in catogary_list:
-        if not result.get_parent():
-            bars.append(result)
-            bar_dict[result.name] = []
+    directory = []
+    directory_dict = {}
 
-    for bar in bars:
-        sub_bars = Category.objects.filter(parent=bar)
-        for i in sub_bars:
-            bar_dict[bar.name].append(i.name)
+    for q in catogary_list:
+        if not q.parent:
+            directory.append(q)
+    directory_name = []
 
+    for dir in directory:
+        directory_name.append(dir.name)
+        directory_dict[dir.name] = []
+        article_tmp = Article.objects.filter(parent=dir)
+        if len(article_tmp)>0:
+            for i in article_tmp:
+                directory_dict[dir.name].append(i.name)
 
 def mainview(request, param1):
     article_list = Article.objects.filter(title=param1)
@@ -40,4 +42,24 @@ def mainview(request, param1):
                'content': mark_safe(article_body),
                'path': article_path,
                }
+    catogary_list = Category.objects.all()
+    directory = []
+    #directory_dict = {}
+
+    for q in catogary_list:
+        if not q.parent:
+            directory.append(q)
+    directory_name = []
+
+    for dir in directory:
+        directory_name.append(dir.name)
+        context[dir.name] = []
+
+        article_tmp = Article.objects.filter(category=dir)
+        if len(article_tmp) > 0:
+            for i in article_tmp:
+                context[dir.name].append(i.title)
+
+    print(directory_name)
+    context['directory_name'] = directory_name
     return render(request, 'mainview.html', context)
